5f9544596a22a1ca98e16a09e4a20d4083928706
Corentin BEUF <corentin.beuf.professionnel@proton.me>
2025-07-06 15:37:17 +0200

    Create script to check if printer job is blocked and alert using mail

---
+++ Printer/GetPrinterJobOnPrinter.ps1
@@ -0,0 +1,39 @@
+# === Parameter ===
+$printers = Get-Printer
+$logFile = "C:\Logs\PrintCleanup_$(Get-Date -Format 'dd-MM-yyyy').log"
+$logDir = "C:\Logs"
+
+if (!(Test-Path $logDir)) { 
+    New-Item -Path $logDir -ItemType Directory 
+}
+
+# === SMTP & mail ===
+$Destinataires = "no_reply@example.com"
+[string[]]$To = $Destinataires.Split(',')
+$smtpServer = "smtp.example.com"
+$subject = "Many printer job are blocked on the server toto"
+$body = "You will find the report attached to this mail"
+
+if (!(Test-Path $logFile)) {
+    New-Item -Path $logFile -ItemType File
+}
+
+foreach ($printer in $printers)
+{
+    $printJobs = Get-PrintJob -PrinterName $printer.Name
+    $printJobsCount = $printJobs.Count
+
+    if ($printJobsCount -ge 50) {
+        Add-Content -Path $logFile -Value "Il y a $printJobsCount impression en attente sur l'imprimante '$($printer.Name)'"
+    } else {
+        Write-Host
+    }
+}
+
+# === Send mail with attachement ===
+if ((Get-Item $logFile).Length -gt 0) {
+    Send-MailMessage -SmtpServer $smtpServer -From "admin@piroux.com" -To $To -Subject $subject -Body $body -Attachments $logFile -BodyAsHtml
+    Remove-Item $logFil
+} else {
+    Remove-Item $logFile
+}
