9c8ff569137139f3fbd9adcda35631786e71c7c0
Corentin BEUF <corentin.beuf.professionnel@proton.me>
2026-01-04 10:54:57 +0100

    Create script to update Wazuh agent

---
+++ Wazuh/Update-WazuhAgent.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+    Update Windows Wazuh agent
+
+.DESCRIPTION
+    Update Windows Wazuh agent
+
+.PARAMETER Version
+    This parameter must contain the version of Wazuh agent that you want
+
+.EXAMPLE
+    .\Update-WazuhAgent.ps1 -Version 4.14.1
+
+.INPUTS
+.OUTPUTS
+
+.NOTES
+    NAME:   Update-WazuhAgent.ps1
+    AUTHOR: Corentin BEUF
+    EMAIL:  corentin.beuf@ynov.com
+    VERSION HISTORY:
+    1.0     2026.01.04
+            Initial version
+#>
+
+# Parameters
+param(
+    $Version
+)
+
+#Requires -Version 5.1
+#Requires -RunAsAdministrator
+
+if (!(Test-Path "C:\Temp")) {
+    Write-Host "`n[INFO] Create temp folder..." -ForegroundColor Cyan
+    New-Item -Path "C:\Temp" -Type Directory | Out-Null
+}
+
+Write-Host "`n[INFO] Download version $version of Wazuh agent..." -ForegroundColor Cyan
+Invoke-WebRequest -Uri "https://packages.wazuh.com/4.x/windows/wazuh-agent-$($Version)-1.msi" -OutFile "C:\Temp\wazuh-agent-$($Version)-1.msi" -UseBasicParsing | Out-Null
+
+Write-Host "`n[INFO] Start update of Wazuh agent..." -ForegroundColor Cyan
+# Start-Process "C:\Temp\wazuh-agent-$($Version)-1.msi" -Wait
+C:\Temp\wazuh-agent-$($Version)-1.msi /q 
+
+if ((Test-Path "C:\Temp")) {
+    Write-Host "`n[INFO] Remove temp folder..." -ForegroundColor Cyan
+    Remove-Item -Path "C:\Temp" -Recurse | Out-Null
+}
